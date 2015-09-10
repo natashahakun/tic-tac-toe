430345129101eab47a073126e6c5f981cb56f68e
natashalynne <hakun.natasha@gmail.com>
2015-09-09 21:28:06 -0400

    Create Players.

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ for (i=0; i<9; i++) {
   div.className = "square";
   div.id = i+1;
 }
+
+var Player = function(firstName, lastName){
+};
+
+var playerOne = new Player();
+var playerTwo = new Player();
+
+playerOne.firstName = prompt("Player One, what is your name?");
+playerTwo.firstName = prompt("Player Two, what is your name?");
+
+alert("Welcome " + playerOne.firstName + " and " + playerTwo.firstName + "!");
